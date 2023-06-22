26bf8d35d4bde65eeba496d0c20b341ada044d68
Isaac A. Pendergrass Jr <isaac@runcanopy.com>
2023-06-21 22:41:02 -0700

    initial checkin

---
+++ transform.go
@@ -0,0 +1,41 @@
+package alchem
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/itchyny/gojq"
+)
+
+func transform(jsonStr, xform string) (string, error) {
+	query, err := gojq.Parse(xform)
+	if err != nil {
+		log.Fatalln(err)
+		return "", err
+	}
+
+	var result map[string]any
+
+	if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
+		fmt.Println(err)
+		return "", nil
+	}
+
+	iter := query.Run(result) // or query.RunWithContext
+	retval := make([]byte, 0)
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if err, ok := v.(error); ok {
+			log.Fatalln(err)
+		}
+		retval, err = json.MarshalIndent(v, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return string(retval), nil
+}
