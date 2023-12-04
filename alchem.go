0381c18e6a532e0a9c2e1b110dbbea496571c5cc
isaacp <isaac.pendergrass@gmail.com>
2023-12-04 08:36:46 -0800

    concern separation

--- alchem.go
+++ alchem.go
@@ -2,34 +2,44 @@ package alchem
 
 import (
 	"encoding/json"
-	"strings"
 
 	"github.com/itchyny/gojq"
 )
 
-func ConvertAndTransform[T any](object any, xform string) (string, error) {
+func TransformObject[T any](object any, xform string) (T, error) {
+	var retVal T
 	jsonBytes, err := json.Marshal(object)
 	if err != nil {
-		return "", err
+		return retVal, err
 	}
-	jsonString := strings.Replace(string(jsonBytes), "\\\"", "", -1)
-	return Transform[T](jsonString, xform)
+	jsonString := string(jsonBytes)
+	jsn, err := TransformJson(jsonString, xform)
+	if err != nil {
+		return retVal, err
+	}
+
+	err = json.Unmarshal([]byte(jsn), &retVal)
+	if err != nil {
+		return retVal, err
+	}
+
+	return retVal, nil
 }
 
-func Transform[T any](jsonStr, xform string) (string, error) {
+func TransformJson(jsonStr, xform string) (string, error) {
 	query, err := gojq.Parse(xform)
 	if err != nil {
 		return "", err
 	}
 
-	//var result T
+	var result []any
 
-	//if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
-	//	return "", err
-	//}
+	if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
+		return "", err
+	}
 
-	iter := query.Run(jsonStr) // or query.RunWithContext
-	retval := make([]byte, 0)
+	iter := query.Run(result)
+	mutant := make([]byte, 0)
 	for {
 		v, ok := iter.Next()
 		if !ok {
@@ -38,10 +48,11 @@ func Transform[T any](jsonStr, xform string) (string, error) {
 		if err, ok := v.(error); ok {
 			return "", err
 		}
-		retval, err = json.MarshalIndent(v, "", "  ")
+		mutant, err = json.Marshal(v)
 		if err != nil {
 			return "", err
 		}
 	}
-	return string(retval), nil
+
+	return string(mutant), nil
 }
